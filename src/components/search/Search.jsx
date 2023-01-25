7d371f8585b8df7b7980c38b62fed3c4cf4bede5
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-01-25 15:40:48 +0600

    search ui done

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+     <input placeholder='Name of the city' className='w-5/6 h-12 mx-auto block my-7 px-4 outline-none border-4 font-roboto font-normal rounded-xl text-slate-400 focus:ring-4 ring-slate-600'/>
+    </div>
+  )
+}
+
+export default Search
